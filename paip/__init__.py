a29c93a1ec86f26440f9132c621dc8cbe5f2e5a4
Andrew Hawker <andrew.r.hawker@gmail.com>
2016-09-12 23:33:13 -0700

    Expose 'parser.parse' and 'parser.parse_gen' as public API.
    These two functions should be the bread and butter for most use cases. Consumers importing this package should be configuring the functionality through a few means:
    
    * visitor implementation
    * group_size
    * delimiter
    * rtl
    
    Callers should generate a concrete visitor implementation that works with the configurable size/delimiter to build objects based on their syntax choice.

--- paip/__init__.py
+++ paip/__init__.py
@@ -7,3 +7,12 @@
     :copyright: (c) 2016 Andrew Hawker.
     :license: Apache 2.0, see LICENSE for more details.
 """
+
+from . import parser
+from .parser import *
+
+
+__all__ = parser.__all__
+
+
+__version__ = '1.0.0'
