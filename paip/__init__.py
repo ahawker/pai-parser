4eca452c7437eb47cf577a284f79004fdb8db8c2
Andrew Hawker <andrew.r.hawker@gmail.com>
2016-09-12 23:26:12 -0700

    Create paip package.

---
+++ paip/__init__.py
@@ -0,0 +1,9 @@
+"""
+    paip
+    ~~~~
+
+    Describe and parse shell-safe strings into a structured syntax.
+
+    :copyright: (c) 2016 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
